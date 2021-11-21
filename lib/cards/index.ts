c0cdfac65efeb3c0c19934e93c847b3be8890d1b
Josh Bowling <josh@balena.io>
2021-11-21 00:24:31 +0000

    Remove license headers
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/cards/index.ts
+++ lib/cards/index.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Balena.io - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
 import { oauthClientOutreach } from './balena/oauth-client-outreach';
 import { oauthProviderOutreach } from './balena/oauth-provider-outreach';
 import { emailSequence } from './contrib/email-sequence';
--- test/integration/webhooks/outreach/index.ts
+++ test/integration/webhooks/outreach/index.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Balena.io - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
 export default {
 	'create-sequence': {
 		expected: require('./create-sequence/expected.json'),
